f62804a809e66b253e521f790ae9d00474f4c708
atultaz <atul.tiwari@me.com>
2022-01-10 17:55:26 +0530

    UI Done

---
+++ data/posts.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    id: 'p1',
+    videoUri:
+      'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+    user: {
+      id: 'u1',
+      username: 'daviddobrik',
+      imageUri:
+        'https://stat2.bollywoodhungama.in/wp-content/uploads/2020/11/WhatsApp-Image-2020-11-21-at-9.12.55-AM.jpeg',
+    },
+    description: 'hahahah oh boy @borat',
+    songName: 'NF - The search',
+    songImage:
+      'https://stat2.bollywoodhungama.in/wp-content/uploads/2020/11/WhatsApp-Image-2020-11-21-at-9.12.55-AM.jpeg',
+    likes: 123,
+    comments: 12,
+    shares: 44,
+  },
+
+  {
+    id: 'p2',
+    videoUri:
+      'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+    user: {
+      id: 'u1',
+      username: 'Test',
+      imageUri:
+        'https://stat2.bollywoodhungama.in/wp-content/uploads/2020/11/WhatsApp-Image-2020-11-21-at-9.12.55-AM.jpeg',
+    },
+    description: 'hahahah oh boy @borat',
+    songName: 'NF - The search',
+    songImage:
+      'https://stat2.bollywoodhungama.in/wp-content/uploads/2020/11/WhatsApp-Image-2020-11-21-at-9.12.55-AM.jpeg',
+    likes: 123,
+    comments: 12,
+    shares: 44,
+  },
+
+  {
+    id: 'p3',
+    videoUri:
+      'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+    user: {
+      id: 'u1',
+      username: 'User 3',
+      imageUri:
+        'https://stat2.bollywoodhungama.in/wp-content/uploads/2020/11/WhatsApp-Image-2020-11-21-at-9.12.55-AM.jpeg',
+    },
+    description: 'hahahah oh boy @borat',
+    songName: 'NF - The search',
+    songImage:
+      'https://stat2.bollywoodhungama.in/wp-content/uploads/2020/11/WhatsApp-Image-2020-11-21-at-9.12.55-AM.jpeg',
+    likes: 123,
+    comments: 12,
+    shares: 44,
+  },
+];
---
+++ src/navigation/homeBottomTabNavigator.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import {Image, Text} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Home from '../screens/Home';
+import Inbox from '../screens/Inbox';
+import Search from '../screens/Search';
+import Upload from '../screens/Upload';
+import Profile from '../screens/Profile';
+
+import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import plusIcon from '../assets/images/plus-icon.png';
+
+const Tab = createBottomTabNavigator();
+
+const HomeBottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        tabStyle: {
+          backgroundColor: '#000',
+        },
+        activeTintColor: '#fff',
+      }}>
+      <Tab.Screen
+        name={'Oye'}
+        component={Home}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Entypo name={'home'} size={25} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={'Search'}
+        component={Search}
+        options={{
+          tabBarIcon: ({color}) => (
+            <AntDesign name={'search1'} size={25} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={'Upload'}
+        component={Upload}
+        options={{
+          tabBarIcon: ({}) => (
+            <Image
+              source={plusIcon}
+              style={{height: 35, resizeMode: 'contain'}}
+            />
+          ),
+          tabBarLabel: () => null,
+        }}
+      />
+      <Tab.Screen
+        name={'Inbox'}
+        component={Inbox}
+        options={{
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcons
+              name={'message-minus-outline'}
+              size={25}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={'Profile'}
+        component={Profile}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Ionicons name={'person-outline'} size={25} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default HomeBottomTabNavigator;
